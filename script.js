f2dcb5d666670d179667597f0d27bdfe390897c7
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-06 13:23:36 -0400

    Add a for loop that adds 16 tiles in each row

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const gridContainer = document.querySelector("#gridContainer")
 
 for (let i = 0; i <= 16; i++) {
      const row = document.createElement("div")
+     row.classList.add("rows")
      gridContainer.appendChild(row)
+
+     for (let j = 0; j <= 16; j++) {
+          const tile = document.createElement("div")
+          tile.classList.add("tiles")
+          row.appendChild(tile)
+     }
 }
 
