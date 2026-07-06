f59bd55ccf58fec28466f70673285eed109bbc46
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-06 18:20:03 -0400

    Add an event listener to every tile on creation that changes its background color and border color

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ for (let i = 1; i <= 16; i++) {
 
      for (let j = 1; j <= 16; j++) {
           const tile = document.createElement("div")
+          tile.addEventListener("mouseenter", () =>  {
+               tile.style.backgroundColor = "blue";
+               tile.style.borderColor = "gray";
+          })
           tile.classList.add("tiles")
           row.appendChild(tile)
-     }
+     }    
 }
 
