2e115c878fe7a68a9114ae230c28a92932d01e56
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-06 17:45:55 -0400

    Use flexbox to order the rows and tiles into a grid format

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const gridContainer = document.querySelector("#gridContainer")
 
-for (let i = 0; i <= 16; i++) {
+for (let i = 1; i <= 16; i++) {
      const row = document.createElement("div")
      row.classList.add("rows")
      gridContainer.appendChild(row)
 
-     for (let j = 0; j <= 16; j++) {
+     for (let j = 1; j <= 16; j++) {
           const tile = document.createElement("div")
           tile.classList.add("tiles")
           row.appendChild(tile)
