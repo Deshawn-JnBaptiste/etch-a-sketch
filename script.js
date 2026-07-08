7cc31db43ef0414719039d9a63306be8e0b6b592
Deshawn-JnBaptiste <jnbaptistedeshawn@gmail.com>
2026-07-08 10:28:28 -0400

    Create functions for each button and their corresponding logic

--- script.js
+++ script.js
@@ -42,13 +42,35 @@ clearButton.addEventListener("click", () => {
      eraserButtonPressed = false;
 })
 
+//Button Functions
+function changeColour(tile) {
+     if (colourButtonPressed) {
+          tile.style.backgroundColor = `${colourPicker.value}`;
+     }
+}
 
+function rainbowMode(tile) {
+     if (rainbowButtonPressed) {
+          const red = Math.floor(Math.random() * 256);
+          const green = Math.floor(Math.random() * 256);
+          const blue = Math.floor(Math.random() * 256);
+          tile.style.backgroundColor = `rgb(${red}, ${green}, ${blue}`;
+     }
 
+}
 
+function eraserMode() {
+     if (eraserButtonPressed) {
+          tile.style.backgroundColor = `white`;
+     }
 
+}
 
-
-
+function clearGrid() {
+     if (clearButtonPressed){
+          createGrid(size)
+     }
+}
 
 
 
